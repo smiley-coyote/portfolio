2b7af0a92b699b884003775ba059e95903209354
smiley-coyote <nick.a.alex@gmail.com>
2018-12-31 11:53:33 -0500

    updated portfolio page

---
+++ assets/javascript/app.js
@@ -0,0 +1,28 @@
+// $(document).ready(function(){
+//   function enter(){
+//     console.log("fired")
+//     $('.intro').fadeOut();
+//     setTimeout(aboutme, 1000);
+//   }
+  
+//   function aboutme(){
+//     $('.about-me').fadeIn();
+//   }
+  
+//   $("#get-started").on("click", function () {
+//     enter();
+  
+// })
+
+// function portfolio(){
+//   $('.portfolio').fadeIn();
+//   $('.header').fadeIn();
+// }
+
+// $("#view-my-work").on("click", function(){
+//   $(".about-me").fadeOut();
+//   setTimeout(portfolio, 1000);
+// })
+
+// })
+
