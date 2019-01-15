30f719e162ebc8577a8a9e13f082a7d1229dfad7
smiley-coyote <nick.a.alex@gmail.com>
2019-01-15 11:53:58 -0500

    added smooth anchor scroll

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,24 @@
-// $(document).ready(function(){
-//   function enter(){
-//     console.log("fired")
-//     $('.intro').fadeOut();
-//     setTimeout(aboutme, 1000);
-//   }
-  
-//   function aboutme(){
-//     $('.about-me').fadeIn();
-//   }
-  
-//   $("#get-started").on("click", function () {
-//     enter();
-  
-// })
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
 
-// function portfolio(){
-//   $('.portfolio').fadeIn();
-//   $('.header').fadeIn();
-// }
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
-// $("#view-my-work").on("click", function(){
-//   $(".about-me").fadeOut();
-//   setTimeout(portfolio, 1000);
-// })
+      // Store hash
+      var hash = this.hash;
 
-// })
-
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
