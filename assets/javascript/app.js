960e843d0ce164dbd3f219652295124e86f23de8
smiley-coyote <nick.a.alex@gmail.com>
2019-03-06 22:30:44 -0500

    deleted other pages and added an intro to index

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,4 +21,126 @@ $(document).ready(function(){
       });
     } // End if
   });
+  var str1 = "<h1>Hello! My name is Nicholas Alex</h1>";
+var str2 = "<p>I am a Full Stack Web Developer!</p>";
+var str3 = "<button>Enter</button>";
+var newArr = str1.split("");
+var number = 0;
+var isElement = false;
+var typing;
+var completedHeading = false;
+var completedParagraph = false;
+var loadingTimer = 5;
+var flashingOn = false;
+var loading;
+
+pageStart();
+
+function pageStart(){
+  var website= sessionStorage.getItem("visited");
+  if(website === null){
+    loading = setInterval(loadingPage, 400);
+  }
+  else{
+    document.getElementById("front-page").style.display = "none";
+    document.getElementById("main-page").style.display = "block";
+  }
+}
+
+
+
+function loadingPage(){
+  if(loadingTimer >= 0 && flashingOn == false){
+  $("#content-here").text("|");
+  loadingTimer--;
+  flashingOn = true;
+  }
+  else if(loadingTimer >= 0 && flashingOn == true){
+    $("#content-here").text("");
+    loadingTimer--;
+    flashingOn = false;
+  }
+  else{
+    clearInterval(loading);
+    typer();
+  }
+}
+
+function typer(){
+  
+  document.getElementById("content-here").innerHTML = "";
+ typing = setInterval(checkItem, 60);
+}
+
+function checkItem() {
+  if (number <= newArr.length) {
+    if (newArr[number] === "<") {
+      isElement = true;
+      myFunction2();
+    }
+    if (newArr[number] === ">") {
+      $("#content-here").append("<span>" + newArr[number]);
+      isElement = false;
+      number++;
+    }
+    else {
+      myFunction2();
+    }
+  } else {
+    if (completedHeading === false && completedParagraph === false) {
+      clearInterval(typing);
+      $("#content-here").append("<br>");
+      newArr = str2.split("");
+      number = 0;
+      typing = setInterval(checkItem, 60);
+      completedHeading = true;
+    } 
+    else if(completedHeading === true && completedParagraph === false){
+      clearInterval(typing);
+      $("#content-here").append("<br>");
+      newArr = str3.split("");
+      number = 0;
+      typing = setInterval(checkItem, 60);
+      completedParagraph = true;
+    }
+    
+    else {
+      clearInterval(typing);
+      document.getElementById("run-button").style.display = "block";
+    }
+
+  }
+}
+
+function myFunction2() {
+  if (isElement === true) {
+    $("#content-here").append("<span>" + newArr[number]);
+    number++;
+  } else {
+    $("#content-here").append(newArr[number]);
+    number++;
+  }
+
+}
+
 });
+
+function runButton(){
+  document.getElementById("run-button").style.display = "none";
+  document.getElementById("content-here").style.display = "none";
+  document.getElementById("content-here2").style.display = "block";
+}
+
+function enterSite() {
+    document.getElementById("content-here").style.display = "none";
+    document.getElementById("content-here2").style.display = "none";
+    document.getElementById("enter-button").style.display = "none";
+    pageStart();
+  
+}
+
+function pageStart() {
+  sessionStorage.setItem("visited", "yes")
+  document.getElementById("front-page").style.display = "none";
+  document.getElementById("main-page").style.display = "block";
+}
